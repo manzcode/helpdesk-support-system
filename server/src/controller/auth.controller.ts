771bf32204472eaf871ae5fe02e438b3255aedfc
Mario Mans <mariomans89@gmail.com>
2023-04-25 11:42:01 +0300

    hot-fix

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -1,91 +1,88 @@
-import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import { createToken } from "../helpers/jwt";
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { createToken } from '../helpers/jwt';
 
 const prisma = new PrismaClient();
 
 export const signInController = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const checkUser = await prisma.user.findFirst({
-      where: {
-        email,
-        password,
-      },
-      select: {
-        id: true,
-      },
-    });
-    if (!checkUser)
-      return res
-        .status(400)
-        .json({ message: "Verifier l'email ou le mot de passe" });
+        const checkUser = await prisma.user.findFirst({
+            where: {
+                email,
+                password,
+            },
+            select: {
+                id: true,
+            },
+        });
+        if (!checkUser) return res.status(400).json({ message: "Verifier l'email ou le mot de passe" });
 
-    const token = createToken(checkUser);
+        const token = createToken(checkUser);
 
-    return res.status(200).json({ message: "successfully log in", token });
-  } catch (error) {
-    return res.status(500).json(error);
-  }
+        return res.status(200).json({ message: 'successfully log in', token });
+    } catch (error) {
+        return res.status(500).json(error);
+    }
 };
 
 export const signUpController = async (req: Request, res: Response) => {
-  try {
-    const { email, password, username, role } = req.body;
+    try {
+        const { email, password, username, role } = req.body;
 
-    const checkUser = await prisma.user.findFirst({
-      where: {
-        email,
-      },
-    });
+        const checkUser = await prisma.user.findFirst({
+            where: {
+                email,
+            },
+        });
 
-    if (checkUser) {
-      return res.status(401).json({ message: "User already exist!" });
+        if (checkUser) {
+            return res.status(401).json({ message: 'User already exist!' });
+        }
+
+        const createUser = await prisma.user.create({
+            data: {
+                email,
+                password,
+                role,
+                username,
+            },
+            select: {
+                id: true,
+            },
+        });
+
+        const token = createToken(createUser);
+
+        return res.status(200).json({
+            message: 'successfully registered',
+            token,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
     }
-
-    const createUser = await prisma.user.create({
-      data: {
-        email,
-        password,
-        role,
-        username,
-      },
-      select: {
-        id: true,
-      },
-    });
-
-    const token = createToken(createUser);
-
-    return res.status(200).json({
-      message: "successfully registered",
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(error);
-  }
 };
 
 export const getAUser = async (req: Request, res: Response) => {
-  const { id } = req.query;
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: id as string,
-      },
-      select: {
-        id: true,
-        email: true,
-        username: true,
-        role: true,
-      },
-    });
-    if (!user) return res.status(404).json({ message: "No user Found" });
-    return res.status(200).json(user);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(error);
-  }
+    const { id } = req.query;
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id as string,
+            },
+            select: {
+                id: true,
+                email: true,
+                username: true,
+                role: true,
+            },
+        });
+        if (!user) return res.status(404).json({ message: 'No user Found' });
+        return res.status(200).json(user);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
 };
--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,300 +1,293 @@
-import { Request, Response } from "express";
-import cldInstance from "../config/cloudinary";
-import { PrismaClient, Prisma } from "@prisma/client";
+import { Request, Response } from 'express';
+import cldInstance from '../config/cloudinary';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 interface FilesId {
-  id: string;
-  cldfileId: string;
+    id: string;
+    cldfileId: string;
 }
 
 export const ticketPostController = async (req: Request, res: Response) => {
-  const { id } = req.query;
-  const files = req.files;
-  const {
-    subject,
-    description,
-    priority,
-    status,
-    assignation,
-    date_limit,
-  } = req.body;
-  const filesId: FilesId[] = [];
-  try {
-    // Insert ticket data into Supabase
-    const prismaQueryTicket = await prisma.ticket.create({
-      data: {
-        subject,
-        description,
-        date_limit,
-        status,
-        priority,
-        assigned: assignation,
-        userId: id as string,
-      },
-      select: {
-        id: true,
-      },
-    });
-    // Upload files to Cloudinary and insert file data into Supabase in parallel using Promise.all
-    const fileUploadPromises = (files as Express.Multer.File[]).map(
-      async (file) => {
-        const result = await cldInstance.uploader.upload(file.path);
-        const prismaQueryFile = await prisma.files.create({
-          data: {
-            url: result.secure_url,
-            userId: id as string,
-            tickets: { connect: { id: prismaQueryTicket?.id } },
-          },
-          select: {
-            id: true,
-          },
+    const { id } = req.query;
+    const files = req.files;
+    const { subject, description, priority, status, assignation, date_limit } = req.body;
+    const filesId: FilesId[] = [];
+    try {
+        // Insert ticket data into Supabase
+        const prismaQueryTicket = await prisma.ticket.create({
+            data: {
+                subject,
+                description,
+                date_limit,
+                status,
+                priority,
+                assigned: assignation,
+                userId: id as string,
+            },
+            select: {
+                id: true,
+            },
         });
-        filesId.push({
-          id: prismaQueryFile?.id,
-          cldfileId: result.public_id,
+        // Upload files to Cloudinary and insert file data into Supabase in parallel using Promise.all
+        const fileUploadPromises = (files as Express.Multer.File[]).map(async (file) => {
+            const result = await cldInstance.uploader.upload(file.path);
+            const prismaQueryFile = await prisma.files.create({
+                data: {
+                    url: result.secure_url,
+                    userId: id as string,
+                    tickets: { connect: { id: prismaQueryTicket?.id } },
+                },
+                select: {
+                    id: true,
+                },
+            });
+            filesId.push({
+                id: prismaQueryFile?.id,
+                cldfileId: result.public_id,
+            });
         });
-      }
-    );
-    await Promise.all(fileUploadPromises);
+        await Promise.all(fileUploadPromises);
 
-    const newTicket = await prisma.ticket.findUnique({
-      where: {
-        id: prismaQueryTicket?.id,
-      },
-      include: {
-        user: {
-          select: {
-            id: true,
-            role: true,
-            username: true,
-          },
-        },
-        _count: {
-          select: {
-            files: true,
-            replies: true,
-          },
-        },
-      },
-    });
+        const newTicket = await prisma.ticket.findUnique({
+            where: {
+                id: prismaQueryTicket?.id,
+            },
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        role: true,
+                        username: true,
+                    },
+                },
+                _count: {
+                    select: {
+                        files: true,
+                        replies: true,
+                    },
+                },
+            },
+        });
 
-    return res.status(201).json(newTicket);
-  } catch (error) {
-    // Delete uploaded files and their corresponding data in Supabase if an error occurs
-    const fileDeletePromises = filesId.map(async (file) => {
-      await cldInstance.uploader.destroy(file.cldfileId);
-      await prisma.files.delete({
-        where: {
-          id: file?.id,
-        },
-      });
-    });
-    await Promise.all(fileDeletePromises);
-    console.log(error);
-    return res.status(500).json({ message: "Server Error" });
-  }
+        return res.status(201).json(newTicket);
+    } catch (error) {
+        // Delete uploaded files and their corresponding data in Supabase if an error occurs
+        const fileDeletePromises = filesId.map(async (file) => {
+            await cldInstance.uploader.destroy(file.cldfileId);
+            await prisma.files.delete({
+                where: {
+                    id: file?.id,
+                },
+            });
+        });
+        await Promise.all(fileDeletePromises);
+        console.log(error);
+        return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 export const replyPostController = async (req: Request, res: Response) => {
-  const { id } = req.query;
-  const files = req.files;
-  const { content, ticketId } = req.body;
-  const filesId: FilesId[] = [];
-  try {
-    // Insert reply data into Supabase
-    const prismaQueryReplies = await prisma.replies.create({
-      data: {
-        content,
-        assigned: id as string,
-        ticketId: ticketId,
-      },
-    });
+    const { id } = req.query;
+    const files = req.files;
+    const { content, ticketId } = req.body;
+    const filesId: FilesId[] = [];
+    try {
+        // Insert reply data into Supabase
+        const prismaQueryReplies = await prisma.replies.create({
+            data: {
+                content,
+                assigned: id as string,
+                ticketId: ticketId,
+            },
+        });
 
-    // Upload files to Cloudinary and insert file data into Supabase in parallel using Promise.all
-    const fileUploadPromises = (files as Express.Multer.File[]).map(
-      async (file) => {
-        const result = await cldInstance.uploader.upload(file.path);
-        const prismaQueryFile = await prisma.files.create({
-          data: {
-            url: result.secure_url,
-            userId: id as string,
-            replies: { connect: { id: prismaQueryReplies.id } },
-          },
-          select: {
-            id: true,
-          },
+        // Upload files to Cloudinary and insert file data into Supabase in parallel using Promise.all
+        const fileUploadPromises = (files as Express.Multer.File[]).map(async (file) => {
+            const result = await cldInstance.uploader.upload(file.path);
+            const prismaQueryFile = await prisma.files.create({
+                data: {
+                    url: result.secure_url,
+                    userId: id as string,
+                    replies: { connect: { id: prismaQueryReplies.id } },
+                },
+                select: {
+                    id: true,
+                },
+            });
+            filesId.push({
+                id: prismaQueryFile?.id,
+                cldfileId: result.public_id,
+            });
         });
-        filesId.push({
-          id: prismaQueryFile?.id,
-          cldfileId: result.public_id,
+        await Promise.all(fileUploadPromises);
+        const newReply = await prisma.replies.findUnique({
+            where: {
+                id: prismaQueryReplies.id,
+            },
+            include: {
+                _count: {
+                    select: {
+                        files: true,
+                    },
+                },
+                assignation: {
+                    select: {
+                        id: true,
+                        role: true,
+                        username: true,
+                    },
+                },
+            },
         });
-      }
-    );
-    await Promise.all(fileUploadPromises);
-    const newReply = await prisma.replies.findUnique({
-      where: {
-        id: prismaQueryReplies.id,
-      },
-      include: {
-        _count: {
-          select: {
-            files: true,
-          },
-        },
-        assignation: {
-          select: {
-            id: true,
-            role: true,
-            username: true,
-          },
-        },
-      },
-    });
-    return res.status(201).json(newReply);
-  } catch (error) {
-    // Delete uploaded files and their corresponding data in Supabase if an error occurs
-    const fileDeletePromises = filesId.map(async (file) => {
-      await cldInstance.uploader.destroy(file.cldfileId);
-      await prisma.files.delete({
-        where: {
-          id: file?.id,
-        },
-      });
-    });
-    await Promise.all(fileDeletePromises);
-    console.log(error);
-    return res.status(500).json({ message: "Server Error" });
-  }
+        return res.status(201).json(newReply);
+    } catch (error) {
+        // Delete uploaded files and their corresponding data in Supabase if an error occurs
+        const fileDeletePromises = filesId.map(async (file) => {
+            await cldInstance.uploader.destroy(file.cldfileId);
+            await prisma.files.delete({
+                where: {
+                    id: file?.id,
+                },
+            });
+        });
+        await Promise.all(fileDeletePromises);
+        console.log(error);
+        return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 export const getAllTicketController = async (req: Request, res: Response) => {
-  try {
-    const tickets = await prisma.ticket.findMany({
-      orderBy: { created_at: "desc" },
-      include: {
-        user: true,
-        _count: {
-          select: {
-            files: true,
-            replies: true,
-          },
-        },
-      },
-      take: 10,
-    });
-    return res.status(200).json(tickets);
-  } catch (error) {
-    return res.status(500).json({ message: "Server Error" });
-  }
+    try {
+        const tickets = await prisma.ticket.findMany({
+            orderBy: { created_at: 'desc' },
+            include: {
+                user: {
+                    select: {
+                        id: true,
+                        username: true,
+                        email: true,
+                        role: true,
+                    },
+                },
+                _count: {
+                    select: {
+                        files: true,
+                        replies: true,
+                    },
+                },
+            },
+            take: 10,
+        });
+        return res.status(200).json(tickets);
+    } catch (error) {
+        return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
-export const getAnUserTicketController = async (
-  req: Request,
-  res: Response
-) => {
-  const { id } = req.query;
-  try {
-    const tickets = await prisma.ticket.findMany({
-      where: { userId: id as string },
-      include: {
-        _count: {
-          select: {
-            files: true,
-            replies: true,
-          },
-        },
-      },
-      orderBy: { created_at: "desc" },
-      take: 10,
-    });
-    return res.status(200).json(tickets);
-  } catch (error) {
-    return res.status(500).json({ message: "Server Error" });
-  }
+export const getAnUserTicketController = async (req: Request, res: Response) => {
+    const { id } = req.query;
+    try {
+        const tickets = await prisma.ticket.findMany({
+            where: { userId: id as string },
+            include: {
+                _count: {
+                    select: {
+                        files: true,
+                        replies: true,
+                    },
+                },
+            },
+            orderBy: { created_at: 'desc' },
+            take: 10,
+        });
+        return res.status(200).json(tickets);
+    } catch (error) {
+        return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 export const closeticket = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.query;
-    await prisma.ticket.update({
-      where: {
-        id: id as string,
-      },
-      data: {
-        status: "closed",
-      },
-      select: {
-        id: true,
-      },
-    });
-    return res.status(200).json({ message: "Ticket closed" });
-  } catch (error) {
-    return res.status(500).json({ message: "Server Error" });
-  }
+    try {
+        const { id } = req.query;
+        await prisma.ticket.update({
+            where: {
+                id: id as string,
+            },
+            data: {
+                status: 'closed',
+            },
+            select: {
+                id: true,
+            },
+        });
+        return res.status(200).json({ message: 'Ticket closed' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 export const viewReplyTicket = async (req: Request, res: Response) => {
-  try {
-    const { ticketid } = req.query;
+    try {
+        const { ticketid } = req.query;
 
-    const ticketReply = await prisma.replies.findMany({
-      where: { ticketId: ticketid as string },
-      include: {
-        _count: {
-          select: {
-            files: true,
-          },
-        },
-        assignation: true,
-      },
-      orderBy: { createdAt: "asc" },
-      take: 10,
-    });
-    return res.status(200).json(ticketReply);
-  } catch (error) {
-    return res.status(500).json({ message: "Server Error" });
-  }
+        const ticketReply = await prisma.replies.findMany({
+            where: { ticketId: ticketid as string },
+            include: {
+                _count: {
+                    select: {
+                        files: true,
+                    },
+                },
+                assignation: true,
+            },
+            orderBy: { createdAt: 'asc' },
+            take: 10,
+        });
+        return res.status(200).json(ticketReply);
+    } catch (error) {
+        return res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 export const getFiles = async (req: Request, res: Response) => {
-  const { ticketid, replyid } = req.query;
-  try {
-    const ticketFiles = await prisma.ticket.findMany({
-      where: {
-        id: ticketid as string,
-      },
-      select: {
-        id: true,
-        files: {
-          select: {
-            id: true,
-            url: true,
-          },
-        },
-      },
-    });
-    const replyFiles = await prisma.replies.findMany({
-      where: {
-        id: replyid as string,
-      },
-      select: {
-        id: true,
-        files: {
-          select: {
-            id: true,
-            url: true,
-          },
-        },
-      },
-    });
+    const { ticketid, replyid } = req.query;
+    try {
+        const ticketFiles = await prisma.ticket.findMany({
+            where: {
+                id: ticketid as string,
+            },
+            select: {
+                id: true,
+                files: {
+                    select: {
+                        id: true,
+                        url: true,
+                    },
+                },
+            },
+        });
+        const replyFiles = await prisma.replies.findMany({
+            where: {
+                id: replyid as string,
+            },
+            select: {
+                id: true,
+                files: {
+                    select: {
+                        id: true,
+                        url: true,
+                    },
+                },
+            },
+        });
 
-    if (ticketid !== undefined) {
-      return res.status(200).json(ticketFiles);
+        if (ticketid !== undefined) {
+            return res.status(200).json(ticketFiles);
+        }
+        return res.status(200).json(replyFiles);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Server Error' });
     }
-    return res.status(200).json(replyFiles);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Server Error" });
-  }
 };
